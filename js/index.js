0e92e69d3138d1a21b478cd02c82a2a216588699
TooCai <18856953069@139.com>
2017-04-16 23:29:49 +0800

    增加layer,jquery

--- js/index.js
+++ js/index.js
@@ -1,68 +1,74 @@
 ;
-(function() {
-	var vm = new Vue({
-		el: "#app",
-		data: {
-			userName: "崔中华",
-			sideInfos: [{
-				title: "Contact. 联系方式",
-				content: [{
-					iconClass: "fa fa-envelope",
-					text: "邮箱:1127254551@qq.com"
-				}, {
-					iconClass: "fa fa-qq",
-					text: "QQ:1127254551"
-				}, {
-					iconClass: "fa fa-mobile fa-2x",
-					text: "Tel:18856953069"
-				}]
-			}, {
-				title: "Application. 应聘岗位",
-				content: [{
-					iconClass:"",
-					text:"前端工程师"
-				}]
-			}, {
-				title: "Tech. 技能点",
-				content: [{
-					iconClass:"",
-					text:"HTML",
-					rate:"width:60%"
-				},{
-					iconClass:"",
-					text:"CSS",
-					rate:"width:40%"
-				},
-				{
-					iconClass:"",
-					text:"JavaScript",
-					rate:"width:65%"
-				},
-				{
-					iconClass:"",
-					text:"Java",
-					rate:"width:30%"
-				}]
-			}],
-			myselfs:[{
-				userK:"姓名",
-				userN:"崔中华"
-			},{
-				userK:"性别",
-				userN:"男"
-			},{
-				userK:"年龄",
-				userN:24
-			},{
-				userK:"学历",
-				userN:"本科"
-			},{
-				userK:"毕业院校",
-				userN:"合肥学院"
-			},{
-				userK:"专业",
-				userN:"计算机科学与技术"
-			}]
+(function($,undefined) {
+	Vue.filter("omit",function(value) {
+		return value.length>30?(value.slice(0,30)+"..."):value;
+	});
+	Vue.filter("listNum",function(value,spaceStr,index){
+		if(spaceStr.length === 1){
+			return (index + 1) + spaceStr + value;
+		}else if(spaceStr.length === 2){
+			var spaceStrs = spaceStr.split("");
+			return spaceStrs[0] + (index + 1) + spaceStrs[1] + value;
 		}
 	});
-})();
+	var vm = new Vue({
+		el: "#app",
+		ready:function(){
+			var urlStr = "data/json.json";
+			var _self = this;
+			var loadIndex = layer.load(3, {shade: [0.3, '#aaa']});
+			$.ajax({
+				url:urlStr,
+				dataType:"json",
+				success:function(data){
+					_self.userName = data.userName;
+					_self.sideInfos = data.sideInfos;
+					_self.myselfs =data.myselfs;
+					_self.projects = data.projects;
+					_self.skills = data.skills;
+					_self.suggest = data.suggest;
+					_self.searchContent = data.searchContent;
+					_self.suggestContent = data.suggestContent;
+					layer.close(loadIndex);
+				},error:function(){
+					layer.close(loadIndex);
+					layer.alert("加载数据失败...");
+				}
+			});
+		},
+		data: {
+			userName:"",
+			sideInfos:[],
+			myselfs:[],
+			projects:[],
+			skills:[],
+			suggest: false,
+			searchContent: "",
+			suggestContent: []
+		},
+		methods:{
+			clickSearch:function(){
+				this.suggest = !this.suggest;
+			},
+			search:function(){
+				if(this.suggestContent.length > 5){
+					this.suggestContent.shift(this.searchContent);
+				}
+				this.suggestContent.push(this.searchContent);
+			},
+			choose:function(e){
+				var index = parseInt(e.target.getAttribute("data-index"));
+				this.searchContent = this.suggestContent[index];
+			},
+			hasEqualInProject:function(index){
+				return index === 0 || !(this.projects[index].company === this.projects[index-1].company);
+			},
+			hasEqualInSkill:function(index){
+				return index === 0 || !(this.skills[index].own === this.skills[index-1].own);
+			},
+			datesFormat:function(title,s,e){
+				return title + "(" + s + "-" + e + ")";
+			}
+		}
+	});
+})($);
